eccf2b00c25618382aa929f4d2ae7d4074de506e
SAURAV KUMAR JHA <mysauravnitw2019@outlook.com>
2021-05-16 10:34:28 +0530

    added movies api

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const genres = require('./routes/genres')
+const movies = require('./routes/movies')
 const home = require('./routes/home')
 
 mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('connected to db..'))
-    .catch((err) => console.error("couldn't connect to db..", err))
+    .catch((err) => {
+        console.error("couldn't connect to db..", err)
+    })
 
 app.use(express.json());
 app.use(express.static('public'));
 app.use('/api/genres', genres)
+app.use('/api/movies', movies)
 app.use('/', home);
 
 const port = process.env.PORT || 3000;
--- routes/genres.js
+++ routes/genres.js
@@ -1,16 +1,7 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
-const Joi = require('joi');
-
-const Genre = mongoose.model('Genre', new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 1,
-        maxlength: 50
-    }
-}));
+const { validateGenre, Genre } = require('../models/genres')
 
 router.get('/', async(req, res) => {
     const genres = await Genre.find().sort('name');
@@ -19,7 +10,7 @@ router.get('/', async(req, res) => {
 
 router.get('/:id', async(req, res) => {
     try {
-        let genre = await Genre.findById(req.params.id);
+        const genre = await Genre.findById(req.params.id);
         if (!genre || genre.length < 1) return res.status(404).send('genre with given id was not found');
         res.status(200).send(genre);
     } catch (err) {
@@ -28,7 +19,7 @@ router.get('/:id', async(req, res) => {
 });
 
 router.post('/', async(req, res) => {
-    const { error } = validateReq(req);
+    const { error } = validateGenre(req);
     if (error) return res.status(400).send(error.details[0].message);
     const genre = new Genre({ name: req.body.name });
     const result = await genre.save();;
@@ -36,7 +27,7 @@ router.post('/', async(req, res) => {
 });
 
 router.put('/:id', async(req, res) => {
-    const { error } = validateReq(req);
+    const { error } = validateGenre(req);
     if (error) return res.status(400).send(error.details[0].message);
     try {
         const result = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, { new: true });
@@ -56,12 +47,5 @@ router.delete('/:id', async(req, res) => {
         res.status(404).send('genre with given id was not found');
     }
 });
-
-function validateReq(req) {
-    const schema = {
-        name: Joi.string().min(1).required()
-    }
-    return Joi.validate(req.body, schema);
-}
 
 module.exports = router;
