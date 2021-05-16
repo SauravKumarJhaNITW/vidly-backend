f0f4e08d43a0da14cbdbe5f98cc011ed826a0192
SAURAV KUMAR JHA <mysauravnitw2019@outlook.com>
2021-05-16 18:55:10 +0530

    bug fixes

---
+++ models/movie.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+const { genreSchema } = require('./genre');
+
+const movieSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1,
+        maxlength: 50
+    },
+    genre: {
+        type: genreSchema,
+        required: true
+    },
+    numberInStock: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 500
+    },
+    dailyRentalRate: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 500
+    }
+})
+const Movie = mongoose.model('Movie', movieSchema);
+
+function validateMovie(req) {
+    const Schema = {
+        title: Joi.string().min(1).required(),
+        genreId: Joi.string().required(),
+        numberInStock: Joi.number().min(0).required(),
+        dailyRentalRate: Joi.number().min(0).required()
+    }
+    return Joi.validate(req.body, Schema);
+}
+
+module.exports = {
+    validateMovie: validateMovie,
+    movieSchema: movieSchema,
+    Movie: Movie
+}
