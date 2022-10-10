3a2f31d9ba2bfd3d4ccdca51a1ebcbfbbc5b128b
RAGHAV MUKATI <mukati_951955@student.nitw.ac.in>
2022-10-10 15:39:13 +0530

    added admin schema

---
+++ models/admin.js
@@ -0,0 +1,53 @@
+const Joi = require("joi");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const adminSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 1024,
+  },
+});
+
+adminSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign(
+    {
+      _id: this._id,
+      name: this.name,
+      email: this.email,
+    },
+    process.env.jwtPrivateKey
+  );
+  return token;
+};
+
+const Admin = mongoose.model("Admin", adminSchema);
+
+function validateUser(req) {
+  const schema = {
+    name: Joi.string().min(1).max(50).required(),
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required(),
+  };
+
+  return Joi.validate(req.body, schema);
+}
+
+exports.Admin = Admin;
+exports.validate = validateUser;
